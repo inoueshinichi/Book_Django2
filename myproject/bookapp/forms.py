afa11fe32518172faad8515ab983ec627faab093
Macbook Air M4 <inoue.shinichi.1800@gmail.com>
2025-10-21 22:45:57 +0900

    [U] Form検証手前まで

---
+++ myproject/bookapp/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Book
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_date', 'cover_image']
+
+        labels = {
+            'title': 'タイトル',
+            'author': '著者',
+            'publication_date': '出版日',
+            'cover_image': '表紙画像',
+        }
