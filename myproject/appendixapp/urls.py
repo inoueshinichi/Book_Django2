293a1658e5a61b0cc5b795c8d2b3c464cb60976f
Macbook Air M4 <inoue.shinichi.1800@gmail.com>
2025-11-14 21:08:35 +0900

    [U] 最後まで終了

---
+++ myproject/appendixapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from . import views
+
+urlpatterns = [
+    # /exe05/
+    path('', TemplateView.as_view(template_name='appendixapp/extends_index.html'), name='extends_index'),
+    path('one/', TemplateView.as_view(template_name='appendixapp/one.html'), name='extends_one'),
+    path('two/', TemplateView.as_view(template_name='appendixapp/two.html'), name='extends_two'),
+    path('samples/', views.sample_list, name='sample_list'),
+    path('samples/<int:id>/', views.sample_detail, name='sample_detail'),
+    path('samples/create/', views.sample_create, name='sample_create'),
+    path('paginator/', views.sample_paginator, name='sample_paginator'),
+]
